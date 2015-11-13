a869ba7be4bd12598f0a1c6b01bd33f00ecdf3a5
Pantelis Sampaziotis <psampaz@gmail.com>
2015-11-13 15:35:27 +0200

    Added RequestIdGeneratorInterace and default implementation

---
+++ src/Symfony/Component/HttpKernel/RequestId/RequestIdGenerator.php
@@ -0,0 +1,10 @@
+<?php
+namespace Symfony\Component\HttpKernel\RequestId;
+
+class RequestIdGenerator implements RequestIdGeneratorInterface
+{
+    public function generate()
+    {
+        return hash('sha256', uniqid(mt_rand(), true));
+    }
+}
