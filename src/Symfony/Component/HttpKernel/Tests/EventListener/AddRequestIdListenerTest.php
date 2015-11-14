59db9d92712bf624a58c62ec069b229ec1f61120
Pantelis Sampaziotis <psampaz@gmail.com>
2015-11-14 22:44:09 +0200

    Renamed RequestIdListener. Added support for custom request id generators

--- src/Symfony/Component/HttpKernel/Tests/EventListener/AddRequestIdListenerTest.php
+++ src/Symfony/Component/HttpKernel/Tests/EventListener/AddRequestIdListenerTest.php
@@ -13,17 +13,44 @@ namespace Symfony\Component\HttpKernel\Tests\EventListener;
 
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpKernel\EventListener\RequestIdListener;
+use Symfony\Component\HttpKernel\EventListener\AddRequestIdListener;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 
-class RequestIdListenerTest extends \PHPUnit_Framework_TestCase
+class AddRequestIdListenerTest extends \PHPUnit_Framework_TestCase
 {
     private $requestStack;
 
     protected function setUp()
     {
         $this->requestStack = $this->getMock('Symfony\Component\HttpFoundation\RequestStack', array(), array(), '', false);
+        $this->generator = $this->getMock('Symfony\Component\HttpKernel\RequestId\RequestIdGeneratorInterface', array(), array(), '', false);
+    }
+
+    public function testAddsRequestIdWithDefaultHeader()
+    {
+        $request = Request::create('/');
+
+        $this->generator->method('generate')->willReturn('Kdxh2uvDfRZDaFTumYu97m5F');
+
+        $listener = new AddRequestIdListener($this->requestStack, $this->generator);
+        $event = $this->getEvent($request);
+
+        $listener->onKernelRequest($event);
+        $this->assertEquals('Kdxh2uvDfRZDaFTumYu97m5F', $request->headers->get('X-Request-Id'));
+    }
+
+    public function testAddsRequestIdWithCustomHeader()
+    {
+        $request = Request::create('/');
+
+        $this->generator->method('generate')->willReturn('VPdtbSQRHQdDyh4qtxVKcf7h');
+
+        $listener = new AddRequestIdListener($this->requestStack, $this->generator, 'X-Request-Id-Custom');
+        $event = $this->getEvent($request);
+
+        $listener->onKernelRequest($event);
+        $this->assertEquals('VPdtbSQRHQdDyh4qtxVKcf7h', $request->headers->get('X-Request-Id-Custom'));
     }
 
     public function testRequestIdIsNotOverridden()
@@ -31,7 +58,7 @@ class RequestIdListenerTest extends \PHPUnit_Framework_TestCase
         $request = Request::create('/');
         $request->headers->set('X-Request-Id', 'exists');
 
-        $listener = new RequestIdListener($this->requestStack);
+        $listener = new AddRequestIdListener($this->requestStack, $this->generator);
         $event = $this->getEvent($request);
 
         $listener->onKernelRequest($event);
@@ -40,7 +67,7 @@ class RequestIdListenerTest extends \PHPUnit_Framework_TestCase
 
     public function testRequestIdInCustomHeader()
     {
-        $listener = new RequestIdListener($this->requestStack, 'Custom-Request-Id');
+        $listener = new AddRequestIdListener($this->requestStack, $this->generator, 'Custom-Request-Id');
         $event = $this->getEvent($request = Request::create('/'));
 
         $listener->onKernelRequest($event);
